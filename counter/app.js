522de19fcf90e6d94dd2746f0cc6a189b71f0876
El Haouni Badr <badr.elhaouni@gmail.com>
2020-10-15 22:57:26 +0200

    initialisation des buttons

---
+++ counter/app.js
@@ -0,0 +1,7 @@
+let counter = 0; // initial count
+// select values of buttons
+
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+console.log(btns);
