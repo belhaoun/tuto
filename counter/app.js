5dfbb8baa01bdfaacb8bdfef19effcd198dece5b
El Haouni Badr <badr.elhaouni@gmail.com>
2020-10-15 23:19:30 +0200

    projet counter finish

--- counter/app.js
+++ counter/app.js
@@ -1,7 +1,30 @@
-let counter = 0; // initial count
+let count = 0; // initial count
 // select values of buttons
 
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
 
 console.log(btns);
+
+btns.forEach(function(btn) {
+    btn.addEventListener("click", function(e) {
+        const styles = e.currentTarget.classList;
+        if(styles.contains('decrease')) {
+            count--;
+        } else if(styles.contains('increase')) {
+            count++;
+        } else {
+            count = 0;
+        }
+        if(count > 0) {
+            value.style.color = "green";
+        }else if (count < 0) {
+            value.style.color = "red";
+        }else {
+            value.style.color = "black";
+        }
+        value.textContent = count;
+    })
+
+
+});
